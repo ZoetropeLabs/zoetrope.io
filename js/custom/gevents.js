4afd423fb7845267268b1f1c198a7b2b37821aca
Ben Howes <ben@ben-howes.co.uk>
2018-03-06 13:42:05 +0000

    added drift events

--- js/custom/gevents.js
+++ js/custom/gevents.js
@@ -13,5 +13,12 @@ const gEvents = (function ($) {
       })
     }
   })
- 
+
+  // Drift related actions
+  if(window.drift){
+    window.drift.on('startConversation', function() {ga('send', 'event', 'Drift Widget', 'Chat Started'); });
+    window.drift.on('emailCapture', function(data) {ga('send', 'event', 'Drift Widget', 'Email Captured'); });
+    window.drift.on('scheduling:meetingBooked', function(data) {ga('send', 'event', 'Drift Widget', 'Meeting Booked'); });
+  }
+
 })(jQuery)
